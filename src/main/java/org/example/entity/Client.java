37c6383f70637a0da1bf72201e8f371fe655938a
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-08-17 03:35:19 +0300

    Create Client.java

---
+++ src/main/java/org/example/entity/Client.java
@@ -0,0 +1,56 @@
+package org.example.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}'+ '\n';
+    }
+
+    public Client() {
+    }
+
+
+
+    public Client(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
