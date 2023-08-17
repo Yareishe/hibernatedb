90292ad36428e880128a3699af0e8f73e5175156
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-08-17 03:35:24 +0300

    Create ClientCrudService.java

---
+++ src/main/java/org/example/ClientCrudService.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.example.entity.Client;
+import org.example.entity.Ticket;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.example.HibernateUtil;
+
+import java.util.List;
+import java.util.Objects;
+
+
+public class ClientCrudService {
+
+    public void createClient(Client client){
+        Session session = HibernateUtil.getConfiguration().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(client);
+        transaction.commit();
+    }
+
+    public void deleteClient(int id){
+        Transaction transaction = null;
+        Session session = HibernateUtil.getConfiguration().openSession();
+            transaction = session.beginTransaction();
+            Client client = session.get(Client.class, id);
+            session.delete(client);
+            transaction.commit();
+    }
+
+
+    public void updateClient(String name,int id){
+        Session session = HibernateUtil.getConfiguration().openSession();
+        Transaction transaction = session.beginTransaction();
+        Client client = session.get(Client.class , id);
+        client.setName(name);
+        session.persist(client);
+        transaction.commit();
+    }
+
+
+
+    public List<Client> readClient() {
+        Session session = HibernateUtil.getConfiguration().openSession();
+            return session.createQuery("FROM Client", Client.class).list();
+    }
+}
