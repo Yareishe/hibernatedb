fd84e0a934fb8341fb177fc6d537aa6653899fdd
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-08-23 04:20:32 +0300

    Update Client.java

--- src/main/java/org/example/entity/Client.java
+++ src/main/java/org/example/entity/Client.java
@@ -4,14 +4,18 @@ package org.example.entity;
 import jakarta.persistence.*;
 import lombok.Builder;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "client")
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JoinColumn(name = "id", referencedColumnName = "client_id",nullable = false)
-    private Long id;
+    private long id;
+    @Column(name = "name")
     private String name;
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+    private Set<Ticket> tickets;
 
     public String getName() {
         return name;
